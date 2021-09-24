86feaac3642c0adb9775b3bb157ece17932105bb
Anton Sorokin <kombatgame@yandex.ru>
2021-09-24 23:20:32 +0300

    remove whitespaces

--- traffic_light/traffic_light.ino
+++ traffic_light/traffic_light.ino
@@ -8,7 +8,7 @@ void setup() {
   pinMode(RED, OUTPUT);
   pinMode(YELLOW, OUTPUT);
   pinMode(GREEN, OUTPUT);
-  
+
   digitalWrite(RED, HIGH);
   digitalWrite(YELLOW, HIGH);
   digitalWrite(GREEN, HIGH);
@@ -17,43 +17,43 @@ void setup() {
   digitalWrite(YELLOW, LOW);
   digitalWrite(GREEN, LOW);
   delay(500);
-  
+
 }
 
 void loop() {
-  digitalWrite(RED, HIGH);   
-  delay(5000);                      
-  digitalWrite(YELLOW, HIGH);    
+  digitalWrite(RED, HIGH);
+  delay(5000);
+  digitalWrite(YELLOW, HIGH);
   delay(1350);
-  
+
   digitalWrite(RED, LOW);
   digitalWrite(YELLOW, LOW);
-  
+
   digitalWrite(GREEN, HIGH);
   delay(5000);
-  
+
   digitalWrite(GREEN, LOW);
   delay(500);
-  
-  digitalWrite(GREEN, HIGH);
-  delay(500);
-  
-  digitalWrite(GREEN, LOW);
-  delay(500);
-  
-  digitalWrite(GREEN, HIGH);
-  delay(500);
-  
-  digitalWrite(GREEN, LOW);
-  delay(500);
-  
+
   digitalWrite(GREEN, HIGH);
   delay(500);
 
   digitalWrite(GREEN, LOW);
-  
+  delay(500);
+
+  digitalWrite(GREEN, HIGH);
+  delay(500);
+
+  digitalWrite(GREEN, LOW);
+  delay(500);
+
+  digitalWrite(GREEN, HIGH);
+  delay(500);
+
+  digitalWrite(GREEN, LOW);
+
   digitalWrite(YELLOW, HIGH);
   delay(1350);
   digitalWrite(YELLOW, LOW);
-  
+
 }
